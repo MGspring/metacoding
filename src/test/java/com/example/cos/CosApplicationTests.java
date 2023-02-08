fd7c53b441d0964a25030e396411e5987c886792
MGspring <wlsaudrhkd1@gmail.com>
2023-02-08 15:19:42 +0900

    day 1

---
+++ src/test/java/com/example/cos/CosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
